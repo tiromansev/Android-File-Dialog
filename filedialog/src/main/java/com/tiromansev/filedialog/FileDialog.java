2e1def6be3b134a4b1a9407632b06b9d21ae829c
tiromansev <tiromansev@gmail.com>
2021-03-22 10:37:51 +0300

    android R support

--- filedialog/src/main/java/com/tiromansev/filedialog/FileDialog.java
+++ filedialog/src/main/java/com/tiromansev/filedialog/FileDialog.java
@@ -109,7 +109,7 @@ public class FileDialog implements IFileDialog {
         this.filterFileExt = filterFileExt;
     }
 
-    private Uri getBaseUri() {
+    public Uri getBaseUri() {
         if (!TextUtils.isEmpty(AppPrefs.basePath().getValue())) {
             return Uri.parse(AppPrefs.basePath().getValue());
         }
